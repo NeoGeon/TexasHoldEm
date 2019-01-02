c116803a0cc4a8692125b240ffa8dc3f8e4dfd0b
NeoGeon <987378754@qq.com>
2019-01-02 13:38:56 +0800

    add texas rules

--- src/texas.cpp
+++ src/texas.cpp
@@ -10,5 +10,190 @@ Player::Player(){
 }
 
 TexasHoldEm::TexasHoldEm(){
+    phase=preFlop;
+    
+}
+
+
+bool TexasHoldEm::CheckWinnerByFold(){
+     int count=0;
+     int index=0;
+     for(vector<Player>::iterator it=playes.begin();it!=players.end();it++){
+         if(it->in_pot){
+             count++;
+             index++;
+         }
+     }
+     if(count==1){
+         winner=index;
+         return true;
+     }
+     return false;
+}
+
+bool TexasHoldEm::CheckWinnerFinal(){
 
 }
+
+vector<Card>& TexasHoldEm::FinalCards(vector<Card> cards){
+     
+
+}
+
+void TexasHoldEm:OneTurn(TexasPhase phase){ //修改public 牌堆，等待用户action
+    // 发牌阶段
+    switch(phase){
+        case preFlop:{   
+            for(int i=0;i!=2;i++)
+                for(int j=0;j!=playerMax;j++){
+                    Card  card=deck.draw(); 
+                    players.push_back(card);
+                }
+            break;
+        }
+        case Flop:{
+            for(int i=0;i!=3;i++){
+                Card card=deck.draw();
+                publicCards.push_back(card);
+            }
+            break;
+        }
+        case Turn:{
+            Card card=deck.draw();
+            publicCards.push_back(card);
+            break;
+        }
+        case River:{
+            Card card=deck.draw();
+            publicCards.push_back(card);
+            break; 
+        }
+        case Final:{
+            break;
+        }
+        default:{
+            // error
+            cout<<"error"<<endl;
+        }
+    }
+
+    // player desicion session
+    int startPos=utg;
+    int index=utg;
+    do{
+       if(checkWinnerByFold())break;
+       TexasAction act=players[startPos].action();       
+       if(act==Raise||act==All_in){
+           startPos=index;
+       }
+       index++;
+    }while(startPos==index);
+
+}
+
+
+void TexasHoldEm::oneRound(){
+    
+  
+}
+
+
+
+/* below is private aux function*/
+
+bool TexasHoldEm::CardNumberIsBigger(Card card1, Card card2){
+    return numberCompair[card1.Number()]>numberCompair[card2.Number()];
+
+}
+
+bool TexasHoldEm::IsStrait(vector<Card> suit, bool sorted){
+    if(!sorted){
+        sorted(suit.begin(), suit.end(), CardNumberIsBigger);
+    }
+    for(vector<Card>::iterator it=suit.begin()+1;it!=suit.end();it++){
+        vector<Card>::iterator it0 = it-1; 
+        if(it0->Number()-it->Number!=1){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool TexasHoldEm::IsStraiFlush(vector<Card> suit, bool sorted){
+    return IsFlush(suit,sorted)&&IsStrait(suit, sorted);
+}
+
+bool TexasHoldEm::IsFlush(vector<Card> suit, bool sorted){
+    for(vector<Card>::iterator it=suit.begin()+1;it!=suit.end();it++){
+        if(it->Color()!=suit.begin()->Color()){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool TexasHoldEm::IsFourOfKind(vector<Card> suit, bool sorted){
+    int a[13];
+    for(vector<Card>::iterator it=suit.begin();it!=suit.end();it++){
+        a[it->Color()]++;
+    }
+    for(int i=0;i!=13;i++){
+        if(a[i]==4)return true;
+    }
+    return false;
+}
+
+bool TexasHoldEm::IsThreeOfKind(vector<Card> suit, bool sorted){
+    int a[13];
+    for(vector<Card>::iterator it=suit.begin();it!=suit.end();it++){
+        a[it->Color()]++;
+    }
+    int three=0;
+    int one=0;
+    for(int i=0;i!=13;i++){
+        if(a[i]==3)three++;
+        if(a[i]==1)one++;
+    }
+    if(three==1&&one==2)return true;
+    return false;
+}
+
+bool TexasHoldEm::IsFullHouse(vector<Card> suit, bool sorted){
+    int a[13];
+    for(vector<Card>::iterator it=suit.begin();it！=suit.end();it++){
+        a[it->Color()]++;
+    }
+    int three=0;
+    int two=0;
+    for(int i=0;i!=13;i++){
+        if(a[i]==3)three++;
+        if(a[i]==2)two++;
+    }
+    if(three==1&&two==1)return true;
+    return false;
+}
+
+int pairCounts(vector<Card>& suit){
+    int a[13];
+    for(vector<Card>::iterator it=suit.begin();it!=suit.end();it++){
+        a[it->Color()]++;
+    }
+    int two=0;
+    for(int i=0;i!=13;i++){
+        if(a[i]==2)two++;
+    }
+    return two;
+}
+
+bool TexasHoldEm::IsTwoPairs(vector<Card> suit, bool sorted){
+    int cnt=pairCounts(suit);
+    if(cnt==2)return true;
+    return false;
+}
+
+bool TexasHoldEm::IsOnePair(vector<Card> suit, bool sorted){
+    int cnt=pairCounts(suit)
+    if(cnt==1)return true;
+    return false;
+}
+
--- src/texas.h
+++ src/texas.h
@@ -10,30 +10,77 @@ enum TexasPhase{
     River,
     Final
 };
+enum TexasAction{
+    Check,
+    Fold,
+    Call,
+    Raise,
+    All_in,
+    All_in_call
+};
+enum TexasSuitPattern{
+    StraitFlush,
+    FourOfKind,
+    Fullhouse,
+    Flush,
+    Strait,
+    TreeOfKind,
+    TwoPairs,
+    OnePair,
+    HighCard
+};
+
 class Player{
 private:
     vector<Cards> hand;
     int chips;
+    bool in_pot;
 public:
     Player();
     ~Player();
     void getCard();
     void addMoney();
+    
     void sit();
 };
 class TexasHoldEm{
 private:
     vector<Player> players;
     vector<Cards> publicCards;
+    map<TexasSuitPattern, int> suitCompare;
+    map<int, int> numberCompare;
     Deck deck;
     int dealer;
+    int utg;
     int sb;
     int bb;
     int sbCount; 
     int tableMin;
-    int tableMax; 
+    int tableMax;
+    int winner; 
     long long pot;
     const static int playerMax=7;
+    bool CardNumberIsBigger(Card card1. Card card2);
+    bool NumberIsBigger(vector<Card> suit1, vector<Card> suit2);
+    bool SuitPatternIsBigger(vector<Card> suit1, vector<Card> suit2);
+    bool SuitPatternIsBigger(TexasSuitPattern p1, TexasSuitPattern p2);
+    TexasSuitPattern SuitPattern(vector<Card> suit);
+
+    bool IsStrait(vector<Card> suit, bool sorted=false);
+    bool IsFlush(vector<Card> suitm bool sorted=false);
+    bool IsFourOfKind(vector<Card> suit, bool sorted=false);
+    bool IsThreeOfKind(vector<Card> suit, bool sorted=false);
+    bool IsTwoPairs(vector<Card> suit, bool sorted=false);
+    bool IsFullHouse(vector<Card> suit, bool sorted=false);
+    bool IsOnePair(vector<Card> suit, bool sorted=false);
+    bool IsStaitFlush(vector<Card> suit. bool sorted=false);
+
+    void initRule();
+    void initTable();
+    bool CheckWinnerByFold();
+    bool CheckWinnerFinal();
+    vector<Card>& FinalCards(vector<Card> cards);
+    void sortCard(vector<Card> suit);
 public:
     TexasHoldEm();
     TexasHoldEm(int playerNum, int sbCount);
